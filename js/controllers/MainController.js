7575f60f00ae97eeda61ba6b6df1069f3089ea11
dibosh <dibosh@newscred.com>
2015-10-02 10:35:58 +0600

    Add tabs

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -2,6 +2,26 @@ angular.module('Controllers')
   .controller('MainCtrl', function ($scope, BasicAPIServiceV1) {
     $scope.data = {};
     $scope.data.loading = true;
+    $scope.selectedTabIndex = 0;
+    $scope.tabs = [
+      {
+        title: 'Recent Activities',
+        isActive: true,
+        template: 'views/partials/recent_activities.html'
+      },
+      {
+        title: 'Insights',
+        isActive: false,
+        template: 'views/partials/insights.html'
+      }
+    ];
+
+    $scope.selectTab = function (index) {
+      $scope.tabs[$scope.selectedTabIndex].isActive = false;
+      $scope.selectedTabIndex = index;
+      $scope.tabs[$scope.selectedTabIndex].isActive = true;
+    };
+
     BasicAPIServiceV1.recentActivities()
       .then(function (result) {
         $scope.recentActivities = result.data.activities;
